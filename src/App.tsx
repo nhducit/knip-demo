7ea924cd2385003bcd8903d86896202572adb97a
nhducit <nhducit@users.noreply.github.com>
2023-12-26 17:09:22 +0700

    add unused type

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { User } from "./type-utils";
+
+const user: User = {
+  id: 1,
+  name: "John Doe",
+  email: "abc@gmail.com",
+  age: 20,
+};
+
+console.log("user", user);
 
 function App() {
   const [count, setCount] = useState(0);
